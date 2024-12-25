7f3274dd3bd44aa9fd8aec482b67b900e4467a28
GW <kowonp@gmail.com>
2024-12-25 23:46:48 +0900

    원시타입과 리터럴타입

---
+++ section02/src/chapter01.ts
@@ -0,0 +1,45 @@
+/**
+ * number
+ */
+let num1: number = 1;
+let num2: number = -1;
+let num3: number = 1.23;
+let num4: number = -1.23;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+/**
+ * string
+ */
+let str1: string = "hello";
+let str2: string = `world`;
+let str3: string = `hello ${str1}`;
+
+/**
+ * boolean
+ */
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+/**
+ * null
+ */
+let null1: null = null;
+
+/**
+ * undefined
+ */
+let undefined1: undefined = undefined;
+
+/**
+ * tsconfig.json > "strictNullChecks": false
+ */
+// let numNull: number = null;
+
+/**
+ * 리터럴 타입
+ */
+let numA: 10 = 10;
+let strA: "hello" = "hello";
+let boolA: true = true;
