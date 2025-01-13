3f064fe82d78838598086c416252ce9dd2cd07a0
GW <kowonp@gmail.com>
2025-01-13 16:35:04 +0900

    제네릭 클래스

---
+++ section07/src/chapter05.ts
@@ -0,0 +1,31 @@
+/**
+ * 제네릭 클래스
+ */
+
+class List<T> {
+  constructor(private list: T[]) {}
+
+  push(value: T) {
+    this.list.push(value);
+  }
+
+  pop() {
+    this.list.pop();
+  }
+
+  print() {
+    console.log(this.list);
+  }
+}
+
+const numberList = new List([1, 2, 3]);
+
+numberList.pop();
+numberList.push(4);
+numberList.print();
+
+const stringList = new List(["a", "b", "c"]);
+
+stringList.pop();
+stringList.push("d");
+stringList.print();
